ae9719f5056e7faea2daf796e3af22a22af0d60e
ceeves96 <caoimherobinson96@gmail.com>
2020-04-27 15:08:42 +0100

    forms and webdriver 1

---
+++ src/FormsAndWebDriver.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class FormsAndWebDriver {
+
+
+    public static void Main(String[] args){
+        findingElements1();
+    }
+
+    public static void findingElements1(){
+        //set up chrome driver
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\6601\\Documents\\Training\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/test/login.html");
+
+        //get the email address web element
+        WebElement email = driver.findElement(By.id("email"));
+
+        //get the password web element
+        WebElement password = driver.findElement(By.name("passwd"));
+
+        email.sendKeys("abcd@gmail.com");
+        password.sendKeys("abcdefgh1jkl");
+
+
+
+    }
+
+
+
+
+}
